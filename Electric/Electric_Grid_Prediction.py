19cb025797c5071ccaaa8ebe3631796c6a9a2747
Okeke-Stephen <113146022+Okeke-Stephen@users.noreply.github.com>
2023-04-18 12:54:27 +1200

    Update Electric_Grid_Prediction.py

--- Electric/Electric_Grid_Prediction.py
+++ Electric/Electric_Grid_Prediction.py
@@ -16,62 +16,62 @@ model = load_model('electgrid')
 st.title("Electric Grid Stability Predictive App")
 st.write('This app was created to predict the stability of electric grids by Okeke')
 
-tau1 = st.number_input('tau1', 
+tau1 = st.number_input(label = 'tau1', 
                                                 min_value=0.3, 
                                                 max_value=10.0, 
                                                 value=5.0)
 
-tau2 = st.number_input('tau2', 
+tau2 = st.number_input(label = 'tau2', 
                                                 min_value=0.3, 
                                                 max_value=10.0, 
                                                 value=5.0)
 
-tau3 = st.number_input('tau3', 
+tau3 = st.number_input(label = 'tau3', 
                                                 min_value=0.3, 
                                                 max_value=10.0, 
                                                 value=5.0)
 
-tau4 = st.number_input('tau4', 
+tau4 = st.number_input(label = 'tau4', 
                                                 min_value=0.3, 
                                                 max_value=10.0, 
                                                 value=5.0)
 
-p1 = st.number_input('p1', 
+p1 = st.number_input(label = 'p1', 
                                         min_value=0.5, 
                                         max_value=10.0, 
                                         value=2.0)
 
-p2 = st.number_input('p2', 
+p2 = st.number_input(label = 'p2', 
                                         min_value=-2.0, 
                                         max_value=-1.0, 
                                         value=-1.0)
 
-p3 = st.number_input('p3', 
+p3 = st.number_input(label = 'p3', 
                                         min_value=-2.0, 
                                         max_value=-1.0, 
                                         value=-1.0)
 
-p4 = st.number_input('p4', 
+p4 = st.number_input(label = 'p4', 
                                         min_value=-2.0, 
                                         max_value=-1.0, 
                                         value=-1.0)
 
-g1 = st.number_input('g1', 
+g1 = st.number_input(label = 'g1', 
                                                 min_value=0.01, 
                                                 max_value=10.0, 
                                                 value=4.0)
 
-g2 = st.number_input('g2', 
+g2 = st.number_input(label = 'g2', 
                                                 min_value=0.01, 
                                                 max_value=10.0, 
                                                 value=4.0)
 
-g3 = st.number_input('g3', 
+g3 = st.number_input(label = 'g3', 
                                                 min_value=0.01, 
                                                 max_value=10.0, 
                                                 value=4.0)
 
-g4 = st.number_input('g4', 
+g4 = st.number_input(label = 'g4', 
                                                 min_value=0.01, 
                                                 max_value=10.0, 
                                                 value=4.0)
@@ -80,7 +80,7 @@ g4 = st.number_input('g4',
 features = {'tau1': tau1, 'tau2':tau2,
             'tau3': tau3, 'tau4': tau4,
             'p1': p1,'p2': p2,'p3': p3,'p4': p4,
-            'g1': g1,'g2': g2,'g3': g3,'g4': g4
+            'g1': g1,'g2': g2,'g3': g3,'g4': g4,
             }
  
 
